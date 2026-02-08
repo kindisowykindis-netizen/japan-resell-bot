6805344ca15083a885080a112703f8c8b0f1a134
kindisowykindis-netizen <kindisowykindis@gmail.com>
2026-02-09 02:32:37 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+WEBHOOK_URL = "https://discord.com/api/webhooks/1470122751745331211/..."
+
+KEYWORDS = [
+    "Louboutin Junior Spikes",
+    "Louboutin Louis Junior",
+    "Louboutin Louis",
+    "Louis Vuitton Belt",
+    "LV Belt"
+]
+
+JP_SIZES = ["27", "27.5", "28"]  # ≈ EU 42–43
+MAX_PRICE_PLN = 1000
+
+JPY_TO_PLN = 0.027  # bezpieczny kurs
+CHECK_INTERVAL = 1800  # 30 minut
