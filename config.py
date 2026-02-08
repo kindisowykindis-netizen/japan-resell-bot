5e211e0c4972f793856283300fec7d1df6c399e1
kindisowykindis-netizen <kindisowykindis@gmail.com>
2026-02-09 03:03:38 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+
 WEBHOOK_URL = "https://discord.com/api/webhooks/1470122751745331211/fCbvG65ipiY9__g8kSacoezblWyWNfKQIxMUKGNrwjVr9qnRhHIBfHyZROXMg3o7ENWV"
 
 KEYWORDS = [
@@ -8,8 +9,8 @@ KEYWORDS = [
     "LV Belt"
 ]
 
-JP_SIZES = ["42", "42.5", "43"]  # ≈ EU 42–43
+EU_SIZES = ["42", "42.5", "43"]
 MAX_PRICE_PLN = 1000
 
-JPY_TO_PLN = 0.027  # bezpieczny kurs
-CHECK_INTERVAL = 60  # 1 minut
+JPY_TO_PLN = 0.027
+CHECK_INTERVAL = 300  # 30 minut
